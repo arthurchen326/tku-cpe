06aac1a21bde9134a1e0c125d3258f240faabd15
Henryjaw <jawhenry@gmail.com>
2018-05-14 23:47:29 +0800

    Add files via upload

---
+++ 106-1/02.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input,maxoccur,freq=0;
+    int log[10]= {0};
+
+    cin>>input;
+    while(input!= -1)
+    {
+        log[input]++;
+        cin>>input;
+    }
+    for(int i=0; i<10; i++)
+    {
+        if(log[i]>=freq)
+        {
+            freq=log[i];
+            maxoccur=i;
+        }
+    }
+
+    cout<<"Digit "<<maxoccur<<" has max occurrences.";
+
+    return 0;
+}
